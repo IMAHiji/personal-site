c46dbc27351e83c4a9a8437e245d43826ebf3dce
Luke Askins <lukeaskins@gmail.com>
2020-06-12 07:37:48 -0500

    Some cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
-
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Luke Askins</h1>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
